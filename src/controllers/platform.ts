c01c72485ca76316086f6911cef4bacd17d7d62c
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-09 04:49:00 -0300

    feat -  implementa controller de test para request post

--- src/controllers/platform.ts
+++ src/controllers/platform.ts
@@ -13,3 +13,8 @@ export const getOnePlatform = (req: Request, res: Response, _next: NextFunction)
   const { id } = req.params;
   res.status(StatusCodes.OK).json({body:'platforms', id});
 };
+
+export const postPlatform = (req: Request, res: Response, _next: NextFunction) => {
+  const { body } = req;
+  res.status(StatusCodes.OK).json({body, post: 'request Post'});
+};
