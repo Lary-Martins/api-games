083fef719af9aced54b6b3c19b4b680d1a17477c
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-12 11:50:33 -0300

    feat - implementa função de getOne no controller

--- src/controllers/PlatformController.ts
+++ src/controllers/PlatformController.ts
@@ -2,13 +2,26 @@ import { Request, Response } from 'express';
 import { PlatformService } from '../services/PlatformService';
 import { PlatformRepository } from '../repositories/PlatformRepository';
 
-  export const getAllPlatform = async( _req: Request, res: Response ) => {
-    try {
-      const platformService = new PlatformService(new PlatformRepository);
-      const response = await platformService.getAll();
-      res.status(response.code).json(response.data);
-    } catch (error) { 
-      const message = error as string;
-      console.log(message)
-    }
+export const getAllPlatform = async( _req: Request, res: Response ) => {
+  try {
+    const platformService = new PlatformService(new PlatformRepository);
+    const response = await platformService.getAll();
+    res.status(response.code).json(response.data);
+  } catch (error) { 
+    const message = error as string;
+    console.log(message)
   }
+}
+
+export const getOnePlatform = async( req: Request, res: Response ) => {
+  const { id } = req.params;
+
+  try {
+    const platformService = new PlatformService(new PlatformRepository);
+    const response = await platformService.getOne(id);
+    res.status(response.code).json(response.data);
+  } catch (error) { 
+    const message = error as string;
+    console.log(message)
+  }
+}
