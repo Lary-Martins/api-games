9476cddcd5778551ccd39dbdfbdcfcc53e9254c0
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-12 11:16:06 -0300

    feat - define interface para a classe PlatformService

--- src/interfaces/IPlatform.ts
+++ src/interfaces/IPlatform.ts
@@ -7,3 +7,8 @@ export interface IPlatformRepository {
   getAll(): Promise<IPlatform[]> | null;
   getOne(id: string): Promise<IPlatform | null> | null;
 }
+
+export interface IPlatformService {
+  getAll(): Promise<IPlatformResponse>;
+  getOne(id: string): Promise<IPlatformResponse>;
+}
