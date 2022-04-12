1079ee41519f674fa3c118119f1b67e7ff4b28b8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-12 11:58:27 -0300

    feat - implementa o método para inserir dados no BD

--- src/repositories/PlatformRepository.ts
+++ src/repositories/PlatformRepository.ts
@@ -1,5 +1,6 @@
 import Platform from '../database/models/platform';
 import { IPlatform, IPlatformRepository } from '../interfaces/IPlatform';
+import { uuid } from 'uuidv4';
 
 export class PlatformRepository implements IPlatformRepository {
   async getAll(): Promise<IPlatform[]> {
@@ -16,6 +17,17 @@ export class PlatformRepository implements IPlatformRepository {
     try {
       const data = await Platform.findByPk(id);
       return data;
+    } catch (error) {
+      const message = error as string;
+      throw new Error(message);
+   }
+  }
+
+  async savePlatform(name: string): Promise<IPlatform | null> {
+    try {
+      const newPlatform = { name, id: uuid() };
+      await Platform.create(newPlatform);
+      return newPlatform;
     } catch (error) {
       const message = error as string;
       throw new Error(message);
