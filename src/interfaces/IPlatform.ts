1540ab72dc36bc9aff6e152617677d12c7d31728
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-12 11:14:09 -0300

    feat - define interface para as funções na classe PlatformRepository

--- src/interfaces/IPlatform.ts
+++ src/interfaces/IPlatform.ts
@@ -2,3 +2,8 @@ export interface IPlatform {
   id: string,
   name: string,
 }
+
+export interface IPlatformRepository {
+  getAll(): Promise<IPlatform[]> | null;
+  getOne(id: string): Promise<IPlatform | null> | null;
+}
