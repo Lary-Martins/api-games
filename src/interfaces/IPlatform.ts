957f27dd1ca17849e5ce8cb8d18f3b3cad752038
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-28 12:41:40 -0300

    feat - add propriedade na interface

--- src/interfaces/IPlatform.ts
+++ src/interfaces/IPlatform.ts
@@ -12,6 +12,7 @@ export interface IPlatformResponse {
 
 export interface IPlatformRepository {
   getAll(): Promise<IPlatform[]>;
+  getByName(name: string): Promise<IPlatform | null>;
   getOne(id: string): Promise<IPlatform | null> | null;
   save(name: string): Promise<IPlatform | null>
 }
